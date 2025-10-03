3e8e0222da1ee287de2946007b56bc67930cab77
Thai Chau Dao <thaichau1712qb@gmail.com>
2025-10-02 23:04:13 -0400

    Sucess v2

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ const app = express();
 const CURRENT_WORKING_DIR = process.cwd();
 
 const frontendDistPath = path.join(CURRENT_WORKING_DIR, 'project', 'dist');
+const indexFilePath = path.join(frontendDistPath, 'index.html');
 
 app.use(express.static(frontendDistPath));
 
-app.get('/*', (req, res) => {
-    const indexFilePath = path.join(frontendDistPath, 'index.html');
+app.get('*', (req, res) => {
+    // Kiểm tra xem yêu cầu có phải là file tĩnh đã được phục vụ chưa
+    if (req.originalUrl.startsWith('/dist/')) {
+        return; // Đã được xử lý bởi express.static, không cần làm gì
+    }
+
+    // Gửi index.html cho mọi đường dẫn khác (dành cho routing của React)
     res.sendFile(indexFilePath, (err) => {
         if (err) {
             console.error('Error serving index.html:', err);
